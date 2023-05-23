bc982ad2c5b646c3f38496603c47912747e3db3b
Alex E <chusitoo@gmail.com>
2023-05-23 07:21:43 -0400

    Add missing bit for building image

--- build-env.sh
+++ build-env.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run --rm -it -v $PWD:/flatbush -w /flatbush --name flatbush flatbush:latest
+image=$(docker image ls --format "{{.Repository}}:{{.Tag}}" flatbush | head -1)
+
+if [ "$image" == "" ] ; then
+  image="flatbush:latest"
+  docker build -t "$image" .
+fi
+
+docker run --rm -it -v "$PWD":/flatbush -w /flatbush --name flatbush "$image"
