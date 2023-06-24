d417b02cd4888361c6ae3e507285150888985222
Alex E <chusitoo@gmail.com>
2023-06-23 21:19:31 -0400

    Fix other minor smells from Sonar except for reinterpret_cast

--- flatbush.h
+++ flatbush.h
@@ -124,62 +124,50 @@ inline uint32_t HilbertXYToIndex(uint32_t n, uint32_t x, uint32_t y)
   x = x << (16 - n);
   y = y << (16 - n);
 
-  uint32_t A, B, C, D;
+  uint32_t A;
+  uint32_t B;
+  uint32_t C;
+  uint32_t D;
 
   // Initial prefix scan round, prime with x and y
-  {
-    uint32_t a = x ^ y;
-    uint32_t b = 0xFFFF ^ a;
-    uint32_t c = 0xFFFF ^ (x | y);
-    uint32_t d = x & (y ^ 0xFFFF);
+  uint32_t a = x ^ y;
+  uint32_t b = 0xFFFF ^ a;
+  uint32_t c = 0xFFFF ^ (x | y);
+  uint32_t d = x & (y ^ 0xFFFF);
+  A = a | (b >> 1);
+  B = (a >> 1) ^ a;
+  C = ((c >> 1) ^ (b & (d >> 1))) ^ c;
+  D = ((a & (c >> 1)) ^ (d >> 1)) ^ d;
 
-    A = a | (b >> 1);
-    B = (a >> 1) ^ a;
+  a = A;
+  b = B;
+  c = C;
+  d = D;
+  A = ((a & (a >> 2)) ^ (b & (b >> 2)));
+  B = ((a & (b >> 2)) ^ (b & ((a ^ b) >> 2)));
+  C ^= ((a & (c >> 2)) ^ (b & (d >> 2)));
+  D ^= ((b & (c >> 2)) ^ ((a ^ b) & (d >> 2)));
 
-    C = ((c >> 1) ^ (b & (d >> 1))) ^ c;
-    D = ((a & (c >> 1)) ^ (d >> 1)) ^ d;
-  }
-
-  {
-    uint32_t a = A;
-    uint32_t b = B;
-    uint32_t c = C;
-    uint32_t d = D;
-
-    A = ((a & (a >> 2)) ^ (b & (b >> 2)));
-    B = ((a & (b >> 2)) ^ (b & ((a ^ b) >> 2)));
-
-    C ^= ((a & (c >> 2)) ^ (b & (d >> 2)));
-    D ^= ((b & (c >> 2)) ^ ((a ^ b) & (d >> 2)));
-  }
-
-  {
-    uint32_t a = A;
-    uint32_t b = B;
-    uint32_t c = C;
-    uint32_t d = D;
-
-    A = ((a & (a >> 4)) ^ (b & (b >> 4)));
-    B = ((a & (b >> 4)) ^ (b & ((a ^ b) >> 4)));
-
-    C ^= ((a & (c >> 4)) ^ (b & (d >> 4)));
-    D ^= ((b & (c >> 4)) ^ ((a ^ b) & (d >> 4)));
-  }
+  a = A;
+  b = B;
+  c = C;
+  d = D;
+  A = ((a & (a >> 4)) ^ (b & (b >> 4)));
+  B = ((a & (b >> 4)) ^ (b & ((a ^ b) >> 4)));
+  C ^= ((a & (c >> 4)) ^ (b & (d >> 4)));
+  D ^= ((b & (c >> 4)) ^ ((a ^ b) & (d >> 4)));
 
   // Final round and projection
-  {
-    uint32_t a = A;
-    uint32_t b = B;
-    uint32_t c = C;
-    uint32_t d = D;
-
-    C ^= ((a & (c >> 8)) ^ (b & (d >> 8)));
-    D ^= ((b & (c >> 8)) ^ ((a ^ b) & (d >> 8)));
-  }
+  a = A;
+  b = B;
+  c = C;
+  d = D;
+  C ^= ((a & (c >> 8)) ^ (b & (d >> 8)));
+  D ^= ((b & (c >> 8)) ^ ((a ^ b) & (d >> 8)));
 
   // Undo transformation prefix scan
-  uint32_t a = C ^ (C >> 1);
-  uint32_t b = D ^ (D >> 1);
+  a = C ^ (C >> 1);
+  b = D ^ (D >> 1);
 
   // Recover index bits
   uint32_t i0 = x ^ y;
