f86a1b6392b31f2882130a86a09f6e234211617d
github-action-benchmark <github@users.noreply.github.com>
2026-01-05 04:31:52 +0000

    add Benchmark (googlecpp) benchmark result for d4ad31004cde10626bc0697560f0c1b881f6e171

--- benchmarks/g++/data.js
+++ benchmarks/g++/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767587293457,
+  "lastUpdate": 1767587512611,
   "repoUrl": "https://github.com/chusitoo/flatbush",
   "entries": {
     "Benchmark": [
@@ -3744,6 +3744,68 @@ window.BENCHMARK_DATA = {
             "value": 424333085.00000066,
             "unit": "ns/iter",
             "extra": "iterations: 2\ncpu: 424305861.5000002 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "chusitoo",
+            "username": "chusitoo"
+          },
+          "committer": {
+            "name": "chusitoo",
+            "username": "chusitoo"
+          },
+          "id": "d4ad31004cde10626bc0697560f0c1b881f6e171",
+          "message": "Implement simd",
+          "timestamp": "2025-12-31T02:16:46Z",
+          "url": "https://github.com/chusitoo/flatbush/pull/5/commits/d4ad31004cde10626bc0697560f0c1b881f6e171"
+        },
+        "date": 1767587511940,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_Index1M",
+            "value": 124695575.00000049,
+            "unit": "ns/iter",
+            "extra": "iterations: 6\ncpu: 124683899.5 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Search10Percent",
+            "value": 238140587.33333352,
+            "unit": "ns/iter",
+            "extra": "iterations: 3\ncpu: 238098173.33333337 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Search1Percent",
+            "value": 35198043.777777836,
+            "unit": "ns/iter",
+            "extra": "iterations: 18\ncpu: 35198368.33333335 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Search001Percent",
+            "value": 2410169.1866197265,
+            "unit": "ns/iter",
+            "extra": "iterations: 284\ncpu: 2409865.9084507036 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Neighbors100",
+            "value": 15797267.772727283,
+            "unit": "ns/iter",
+            "extra": "iterations: 44\ncpu: 15795259.431818174 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_NeighborsAll",
+            "value": 112482353.66666628,
+            "unit": "ns/iter",
+            "extra": "iterations: 6\ncpu: 112465180.49999994 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Neighbors1",
+            "value": 418151736.5000005,
+            "unit": "ns/iter",
+            "extra": "iterations: 2\ncpu: 418125458.50000006 ns\nthreads: 1"
           }
         ]
       }
