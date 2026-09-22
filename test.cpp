7235d4fb41cb7ed5d3fb15d6e5e1516135d4a73a
Alex Emirov <chusitoo@gmail.com>
2026-09-22 20:32:28 +0000

    Remove ineffective builder trim test

--- test.cpp
+++ test.cpp
@@ -508,23 +508,6 @@ TEST(FlatbushTest, ReturnIndexOfNewlyAddedRectangle) {
   }
 }
 
-TEST(FlatbushTest, TrimOverallocatedBuilder) {
-  constexpr uint32_t wNumAdded = 50;
-  flatbush::FlatbushBuilder<double> wBuilder(gData.size() / 4);
-
-  for (size_t wIdx = 0; wIdx < static_cast<size_t>(wNumAdded) * 4; wIdx += 4) {
-    wBuilder.add({ gData[wIdx], gData[wIdx + 1], gData[wIdx + 2], gData[wIdx + 3] });
-  }
-
-  wBuilder.trim();
-  auto wIndex = wBuilder.finish();
-  auto wExpected = createSmallIndex(wNumAdded, flatbush::gDefaultNodeSize);
-
-  EXPECT_EQ(wIndex.numItems(), wNumAdded);
-  EXPECT_EQ(wIndex.data().size(), wExpected.data().size());
-  EXPECT_TRUE(std::equal(wIndex.data().begin(), wIndex.data().end(), wExpected.data().begin()));
-}
-
 TEST(FlatbushTest, TrimMovedFromBuilder) {
   flatbush::FlatbushBuilder<int32_t> wBuilder(100);
   wBuilder.add({ 1, 1, 1, 1 });
